561b44e56521be35072351ba5565feaba6344c9f
Jeff Hajewski <jeff.hajewski@gmail.com>
2017-10-04 07:55:09 -0500

    Finishes work on hashing salted password

--- password-hash/src/main.rs
+++ password-hash/src/main.rs
@@ -2,34 +2,35 @@
 extern crate error_chain;
 extern crate ring;
 
+use std::str;
+
 use ring::{digest, hmac, rand};
 use ring::rand::SecureRandom;
 
 error_chain! {
     foreign_links {
         Unspecified(ring::error::Unspecified);
+        Utf8Error(std::str::Utf8Error);
     }
 
 }
 
 fn run() -> Result<()> {
-    // Create password
     let password = "password";
 
-    // Create salt
+    // Create salted password
     let rng = rand::SystemRandom::new();
-    let mut salt = [0u8; 64];
-    rng.fill(&mut salt)?;
+    let mut salt_bytes = [0u8; 64];
+    rng.fill(&mut salt_bytes)?;
+    let salted_password = [&salt_bytes, password.as_bytes()].concat();
     
     // Create signing key for HMAC
-    let signing_key = hmac::SigningKey::generate(&digest::SHA512, salt.as_ref())?;
+    let signing_key = hmac::SigningKey::generate(&digest::SHA512, &rng)?;
     
     // Hash password using HMAC
-    let hash = hmac::sign(&signing_key, password.as_bytes());
+    let hash = hmac::sign(&signing_key, &salted_password);
     println!("Hash: {:?}", hash);
 
-    // Verify passowrd and hash
-    println!("Hello, world!");
     Ok(())
 }
 
