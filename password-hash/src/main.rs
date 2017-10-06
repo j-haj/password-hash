75a6cd16c2d4dfa5d67575d807e752cc69f6b47a
Jeff Hajewski <jeff.hajewski@gmail.com>
2017-10-06 14:29:57 -0500

    Initial work on PBKDF2 hash

--- password-hash/src/main.rs
+++ password-hash/src/main.rs
@@ -4,7 +4,7 @@ extern crate ring;
 
 use std::str;
 
-use ring::{digest, hmac, rand};
+use ring::{digest, hmac, rand, pbkdf2};
 use ring::rand::SecureRandom;
 
 error_chain! {
@@ -18,6 +18,9 @@ error_chain! {
 fn run() -> Result<()> {
     let password = "password";
 
+    // ------------------------------------------------------------------------
+    // HMAC
+    // ------------------------------------------------------------------------
     // Create salted password
     let rng = rand::SystemRandom::new();
     let mut salt_bytes = [0u8; 64];
@@ -31,6 +34,20 @@ fn run() -> Result<()> {
     let hash = hmac::sign(&signing_key, &salted_password);
     println!("Hash: {:?}", hash);
 
+    // ------------------------------------------------------------------------
+    // PBKF2
+    // ------------------------------------------------------------------------
+    static DIGEST_ALG: &'static digest::Algorithm = &digest::SHA512; // What is going on here
+    const CREDENTIAL_LEN: usize = digest::SHA512_OUTPUT_LEN;
+    const N_ITER: u32 = 50;
+
+    let mut salt = [0u8; CREDENTIAL_LEN];
+    let mut pbkdf2_hash = [0u8; CREDENTIAL_LEN];
+
+    rng.fill(&mut salt);
+    pbkdf2::derive(DIGEST_ALG, N_ITER, &salt_bytes, password.as_bytes(),
+        &mut pbkdf2_hash);
+    println!("PBKF2 hash: {:?}", String::from_utf8_lossy(&pbkdf2_hash));
     Ok(())
 }
 
